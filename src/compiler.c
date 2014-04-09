2002acc82bfcef1b160adde2dfcc4ece67b1d43a
Julian Maurice <jajm@jajm.org>
2014-04-09 23:55:35 +0200

    Refactor compiler code

--- src/compiler.c
+++ src/compiler.c
@@ -23,98 +23,177 @@
 #include <sds.h>
 #include "io_embody.h"
 
-enum io_compile_mode {
-	IO_COMPILE_MODE_TEXT,
-	IO_COMPILE_MODE_LUA,
-	IO_COMPILE_MODE_LUA_EXPR,
-	IO_COMPILE_MODE_STRING_SINGLE,
-	IO_COMPILE_MODE_STRING_DOUBLE,
-	IO_COMPILE_MODE_STRING_MULTILINE
-};
+typedef struct {
+	sds buffer;
+	const char *start_tag;
+	const char *end_tag;
+	size_t start_tag_len;
+	size_t end_tag_len;
+	unsigned int multiline_equal_signs_count;
+} io_compile_context_t;
 
-char * io_compile(const char *template, const char *start_tag,
-	const char *end_tag)
+static const char * io_compile_string_single(const char *ptr,
+	io_compile_context_t *context)
 {
-	sds buf = sdsnew("Io.output(\"");
-	char cbuf[2] = "\0\0";
-	char *out;
-	const char *ptr = template;
-	enum io_compile_mode mode = IO_COMPILE_MODE_TEXT;
-	enum io_compile_mode previous_mode = IO_COMPILE_MODE_TEXT;
-	size_t start_tag_len = strlen(start_tag);
-	size_t end_tag_len = strlen(end_tag);
-	size_t len;
-	unsigned int n_equals = 0; // Number of equals signs in multiline string
+	sds *buf = &(context->buffer);
 
-	while (*ptr != '\0') {
-		if (mode == IO_COMPILE_MODE_TEXT && *ptr == '\n') {
-			buf = sdscat(buf, "\\n\");\nIo.output(\"");
-		} else if (mode == IO_COMPILE_MODE_TEXT && *ptr == '"') {
-			buf = sdscat(buf, "\\\"");
-		} else if (mode == IO_COMPILE_MODE_TEXT && strncmp(ptr, start_tag, start_tag_len) == 0) {
-			buf = sdscat(buf, "\");");
-			if (ptr[start_tag_len] == '=') {
-				mode = IO_COMPILE_MODE_LUA_EXPR;
-				buf = sdscat(buf, "Io.output(");
-				ptr += start_tag_len;
-			} else {
-				mode = IO_COMPILE_MODE_LUA;
-				ptr += start_tag_len - 1;
-			}
-		} else if ((mode == IO_COMPILE_MODE_LUA || mode == IO_COMPILE_MODE_LUA_EXPR) && *ptr == '"') {
-			previous_mode = mode;
-			mode = IO_COMPILE_MODE_STRING_DOUBLE;
-			buf = sdscat(buf, "\"");
-		} else if (mode == IO_COMPILE_MODE_STRING_DOUBLE && *ptr == '"' && *(ptr-1) != '\\') {
-			mode = previous_mode;
-			buf = sdscat(buf, "\"");
-		} else if ((mode == IO_COMPILE_MODE_LUA || mode == IO_COMPILE_MODE_LUA_EXPR) && *ptr == '\'') {
-			previous_mode = mode;
-			mode = IO_COMPILE_MODE_STRING_SINGLE;
-			buf = sdscat(buf, "'");
-		} else if (mode == IO_COMPILE_MODE_STRING_SINGLE && *ptr == '\'' && *(ptr-1) != '\\') {
-			mode = previous_mode;
-			buf = sdscat(buf, "'");
-		} else if ((mode == IO_COMPILE_MODE_LUA || mode == IO_COMPILE_MODE_LUA_EXPR) && *ptr == '[') {
-			n_equals = 0;
-			while (*(ptr+n_equals+1) == '=') n_equals++;
-			if (*(ptr+n_equals+1) == '[') {
-				previous_mode = mode;
-				mode = IO_COMPILE_MODE_STRING_MULTILINE;
-			}
-			buf = sdscat(buf, "[");
-		} else if (mode == IO_COMPILE_MODE_STRING_MULTILINE && *ptr == ']') {
-			unsigned int n_equals_count = 0;
-			while (*(ptr+n_equals_count+1) == '=') n_equals_count++;
-			if (n_equals_count == n_equals && *(ptr+n_equals+1) == ']') {
-				mode = previous_mode;
-			}
-			buf = sdscat(buf, "]");
-		} else if (mode == IO_COMPILE_MODE_LUA && strncmp(ptr, end_tag, end_tag_len) == 0) {
-			buf = sdscat(buf, "Io.output(\"");
-			mode = IO_COMPILE_MODE_TEXT;
-			ptr += end_tag_len - 1;
-		} else if (mode == IO_COMPILE_MODE_LUA_EXPR && strncmp(ptr, end_tag, end_tag_len) == 0) {
-			buf = sdscat(buf, "); Io.output(\"");
-			mode = IO_COMPILE_MODE_TEXT;
-			ptr += end_tag_len - 1;
+	while (*ptr) {
+		if (*ptr == '\'' && *(ptr - 1) != '\\') {
+			*buf = sdscat(*buf, "'");
+			break;
 		} else {
-			cbuf[0] = *ptr;
-			buf = sdscat(buf, cbuf);
+			*buf = sdscatlen(*buf, ptr, 1);
 		}
 		ptr++;
 	}
 
-	if (mode == IO_COMPILE_MODE_TEXT) {
-		buf = sdscat(buf, "\")");
-	} else if (mode == IO_COMPILE_MODE_LUA_EXPR) {
-		buf = sdscat(buf, ")");
+	return ptr;
+}
+
+static const char * io_compile_string_double(const char *ptr,
+	io_compile_context_t *context)
+{
+	sds *buf = &(context->buffer);
+
+	while (*ptr) {
+		if (*ptr == '"' && *(ptr - 1) != '\\') {
+			*buf = sdscat(*buf, "\"");
+			break;
+		} else {
+			*buf = sdscatlen(*buf, ptr, 1);
+		}
+		ptr++;
 	}
 
-	len = sdslen(buf);
+	return ptr;
+}
+
+static const char * io_compile_string_multiline(const char *ptr,
+	io_compile_context_t *context)
+{
+	sds *buf = &(context->buffer);
+	unsigned int n_equals = context->multiline_equal_signs_count;
+	unsigned int n_equals_count;
+
+	while (*ptr) {
+		if (*ptr == ']') {
+			n_equals_count = 0;
+			while (*(ptr + n_equals_count + 1) == '=') {
+				n_equals_count++;
+			}
+
+			if (n_equals_count == n_equals && *(ptr + n_equals + 1) == ']') {
+				*buf = sdscatlen(*buf, ptr, n_equals + 2);
+				ptr += n_equals + 1;
+				break;
+			} else {
+				*buf = sdscat(*buf, "]");
+			}
+		} else {
+			*buf = sdscatlen(*buf, ptr, 1);
+		}
+		ptr++;
+	}
+
+	return ptr;
+}
+
+static const char * io_compile_lua(const char *ptr,
+	io_compile_context_t *context)
+{
+	sds *buf = &(context->buffer);
+	const char *end_tag = context->end_tag;
+	size_t end_tag_len = context->end_tag_len;
+	int is_lua_expr = 0;
+
+	if (*ptr == '=') {
+		is_lua_expr = 1;
+		*buf = sdscat(*buf, "Io.output(");
+		ptr++;
+	}
+
+	while (*ptr) {
+		if (*ptr == '\'') {
+			*buf = sdscat(*buf, "'");
+			ptr = io_compile_string_single(ptr + 1, context);
+		} else if (*ptr == '"') {
+			*buf = sdscat(*buf, "\"");
+			ptr = io_compile_string_double(ptr + 1, context);
+		} else if (*ptr == '[') {
+			int n_equals = 0;
+			while (*(ptr + n_equals + 1) == '=') n_equals++;
+			if (*(ptr + n_equals + 1) == '[') {
+				*buf = sdscatlen(*buf, ptr, n_equals + 2);
+				context->multiline_equal_signs_count = n_equals;
+				ptr = io_compile_string_multiline(
+					ptr + n_equals + 2, context);
+			} else {
+				*buf = sdscat(*buf, "[");
+			}
+		} else if (!strncmp(ptr, end_tag, end_tag_len)) {
+			ptr += end_tag_len - 1;
+			break;
+		} else {
+			*buf = sdscatlen(*buf, ptr, 1);
+		}
+		ptr++;
+	}
+
+	if (is_lua_expr) {
+		*buf = sdscat(*buf, ");");
+	}
+
+	return ptr;
+}
+
+static const char * io_compile_text(const char *ptr,
+	io_compile_context_t *context)
+{
+	sds *buf = &(context->buffer);
+	const char *start_tag = context->start_tag;
+	size_t start_tag_len = context->start_tag_len;
+
+	*buf = sdscat(*buf, "Io.output(\"");
+	while (*ptr) {
+		if (*ptr == '\n') {
+			*buf = sdscat(*buf, "\\n\");\nIo.output(\"");
+		} else if (*ptr == '"') {
+			*buf = sdscat(*buf, "\\\"");
+		} else if (!strncmp(ptr, start_tag, start_tag_len)) {
+			*buf = sdscat(*buf, "\");");
+			ptr = io_compile_lua(ptr + start_tag_len, context);
+			*buf = sdscat(*buf, "Io.output(\"");
+		} else {
+			*buf = sdscatlen(*buf, ptr, 1);
+		}
+		ptr++;
+	}
+	*buf = sdscat(*buf, "\");");
+
+	return ptr;
+}
+
+char * io_compile(const char *template, const char *start_tag,
+	const char *end_tag)
+{
+	char *out;
+	const char *ptr = template;
+	size_t len;
+
+	io_compile_context_t context;
+	context.buffer = sdsempty(),
+	context.start_tag = start_tag;
+	context.end_tag = end_tag;
+	context.start_tag_len = strlen(start_tag);
+	context.end_tag_len = strlen(end_tag);
+	context.multiline_equal_signs_count = 0;
+
+	io_compile_text(ptr, &context);
+
+	len = sdslen(context.buffer);
 	out = malloc(sizeof(char) * (len+1));
-	strncpy(out, buf, len+1);
-	sdsfree(buf);
+	strncpy(out, context.buffer, len+1);
+	sdsfree(context.buffer);
 
 	return out;
 }
