087812e136767f01ab0d91f1f2dd468b29260c52
Julian Maurice <jajm@jajm.org>
2014-04-13 14:37:58 +0200

    Add tests for parser

--- t/parser.t.c
+++ t/parser.t.c
@@ -16,20 +16,62 @@ static void test_parser_parse(const char *tpl, const char *exp, const char *msg)
 	sdsfree(code);
 }
 
+#define to_s(s) #s
+
 static void test_simple_text(void)
 {
 	const char *tpl = "foo bar baz";
-	const char *exp = "Io.output(\"foo\");Io.output(\" \");"
-		"Io.output(\"bar\");Io.output(\" \");"
-		"Io.output(\"baz\");";
+	const char *exp = to_s( Io.output("foo");Io.output(" "); )
+		to_s( Io.output("bar");Io.output(" "); )
+		to_s( Io.output("baz"); );
 
 	test_parser_parse(tpl, exp, __func__);
 }
 
 static void test_simple_expr(void)
 {
-	const char *tpl = "#{= \"bar baz\" }#";
-	const char *exp = "Io.output( \"bar baz\" );";
+	const char *tpl = to_s( #{= "bar baz" }# );
+	const char *exp = to_s( Io.output( "bar baz" ); );
+
+	test_parser_parse(tpl, exp, __func__);
+}
+
+static void test_simple_lua(void)
+{
+	const char *tpl = to_s( #{ "bar baz" }# );
+	const char *exp = " \"bar baz\" ";
+
+	test_parser_parse(tpl, exp, __func__);
+}
+
+static void test_simple_text_with_quotes(void)
+{
+	const char *tpl = to_s( 'foo' "bar" [[baz]] );
+	const char *exp = to_s( Io.output("'foo'");Io.output(" ");)
+		to_s( Io.output("\"bar\"");Io.output(" ");)
+		to_s( Io.output("[[baz]]"); );
+
+	test_parser_parse(tpl, exp, __func__);
+}
+
+static void test_newlines(void)
+{
+	const char *tpl = "foobar\n"
+		"\n"
+		"baz\n"
+		"\n"
+		"#{ luacode\n"
+		"\n"
+		"luacode2;}#foo#{= luaexpr\n"
+		"luaexpr2}#";
+	const char *exp = to_s( Io.output("foobar");Io.output("\n"); ) "\n"
+		to_s( Io.output("\n"); ) "\n"
+		to_s( Io.output("baz");Io.output("\n"); ) "\n"
+		to_s( Io.output("\n"); ) "\n"
+		" luacode\n"
+		"\n"
+		to_s( luacode2;Io.output("foo"); ) "Io.output( luaexpr\n"
+		"luaexpr2);";
 
 	test_parser_parse(tpl, exp, __func__);
 }
@@ -44,10 +86,13 @@ static void test_unterminated_string(void)
 
 int main()
 {
-	plan(3);
+	plan(6);
 
 	test_simple_text();
 	test_simple_expr();
+	test_simple_lua();
+	test_simple_text_with_quotes();
+	test_newlines();
 	test_unterminated_string();
 
 	return 0;
